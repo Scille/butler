c74882fc11b8ec1c8ca88a1320661164d472c0f0
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2016-03-07 13:04:17 +0100

    Initial commit

---
+++ tests/test_mestr.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_mestr
+----------------------------------
+
+Tests for `mestr` module.
+"""
+
+import unittest
+
+from mestr import mestr
+
+
+class TestMestr(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(unittest.main())
