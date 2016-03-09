fc3a6d45f17e4c8ad1256f36c05032b9a46eb15d
Loic Rowe <rowe.loic@gmail.com>
2016-03-09 13:01:49 +0100

    transform the mestr in an app ready to be launch by crossbar

--- mestr/mestr.py
+++ mestr/mestr.py
@@ -1,20 +1,24 @@
-from autobahn.twisted.wamp import Application
+from twisted.internet import defer
+from autobahn.wamp.exception import ApplicationError
+from autobahn.twisted.wamp import ApplicationSession
 
-app = Application("mestr")
-
+app = {}
 app._start = {}
 app._waiting = {}
+app._new_app = False
 
 
 def _start_app(application_name, version):
     global app
     app._start[application_name] = version
-    app.session.publish('mestr.' + application_name + '.start')
+    if application_name in app._waiting:
+        app._waiting[application_name]['defer'].callback()
     for key in app._waiting:
         if key not in app._start:
             application = app._waiting[key]
             _try_to_start_app(
                 application['name'], application['version'], application['required_components'])
+    return True
 
 
 def _add_to_waiting_list(application_name, version, required_components):
@@ -23,50 +27,69 @@ def _add_to_waiting_list(application_name, version, required_components):
     if application_name in app._waiting:
         del app._waiting[application_name]
     app._waiting[application_name] = {
-        'name': application_name, 'version': version, 'required_components': required_components}
+        'name': application_name, 'version': version, 'required_components': required_components, 'defer': None}
+    return False
 
 
 def _try_to_start_app(application_name, version, required_components):
     global app
     if not required_components:
-        _start_app(application_name, version)
+        return _start_app(application_name, version)
     else:
         start_component = []
         for key in required_components.keys():
             if key in app._start and required_components[key] == app._start[key]:
                 start_component.append(key)
         if len(start_component) == len(required_components):
-            _start_app(application_name, version)
+            return _start_app(application_name, version)
         else:
-            _add_to_waiting_list(application_name, version, required_components)
+            return _add_to_waiting_list(application_name, version, required_components)
 
 
-@app.register()
-def register(application_name, version, required_components=None):
-    """
-    application_name : name of your application
-    version : version of your application
-    required_components dictionary of components required for you application
-    and their version required
+class MestrSession(ApplicationSession):
 
-        {
-           "component" : "1.1",
-           "component2" : "0.1",
-           ...
-        }
+    def onJoin(self, details):
+        def authenticate(realm, authid, details):
+            """
+            application_name : name of your application
+            version : version of your application
+            required_components dictionary of components required for you application
+            and their version required
 
-     the mestr will publish an event:
+                {
+                   "component" : "1.1",
+                   "component2" : "0.1",
+                   ...
+                }
 
-         mestr.your_application_name.start
+             when all the different component required has been register your component will
+             be allow to authenticate with a role build only for your application with
+             only the right right for it to works
+             """
 
-     when all the different component required has been register
-     """
-    global app
-    _try_to_start_app(application_name, version, required_components)
-    for k in app._start:
-        if k in app._waiting:
-            del app._waiting[k]
+            global app
+            ticket = details['ticket']
+            if 'application_name' not in ticket and 'version' not in ticket:
+                raise ApplicationError(
+                    'could not start the authentication of an app, field application_name or version is missing')
+            application_name = ticket['application_name']
+            version = ticket['version']
+            required_components = ticket[
+                'required_components'] if 'required_components' in ticket else {}
+            if not _try_to_start_app(application_name, version, required_components):
+                ready_defered = defer.Deferred()
+                ready_defered.addCallback(_try_to_start_app,
+                                          application_name=application_name,
+                                          version=version,
+                                          required_components=required_components)
+                app._waiting[application_name]['defer'] = ready_defered
+                yield ready_defered
 
+            for k in app._start:
+                if k in app._waiting:
+                    del app._waiting[k]
 
-if __name__ == '__main__':
-    app.run(url="ws://127.0.0.1:8080/ws")
+            # fake role for the moment must be contains in the ticket
+            return ticket['role']
+
+        return self.register(authenticate, 'mestr.authenticate')
