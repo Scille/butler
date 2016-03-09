780631838eed8770ddcc5e917173aa55a60d3690
Loic Rowe <rowe.loic@gmail.com>
2016-03-09 17:25:00 +0100

    Bump version: 1.0.2 → 1.0.3

--- mestr/__init__.py
+++ mestr/__init__.py
@@ -2,6 +2,6 @@ from .mestr import MestrSession
 
 __author__ = 'Scille SAS'
 __email__ = 'contact@scille.fr'
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
 __all__ = ('MestrSession', )
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='mestr',
-    version='1.0.2',
+    version='1.0.3',
     description="Bring dynamic component registration to wamp crossbar router.",
     long_description=readme,
     author="Scille SAS",
